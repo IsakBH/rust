99b06d9e49e411a4a12311287932c30a4ce247dc
IsakBH <89648535+IsakBH@users.noreply.github.com>
2025-01-09 14:14:12 +0100

    Delete build directory

--- build/libc-0bf4ed26e03f527c/build_script_build-0bf4ed26e03f527c.d
+++
@@ -1,5 +0,0 @@
-/home/isak/Programs/rust/target/debug/build/libc-0bf4ed26e03f527c/build_script_build-0bf4ed26e03f527c: /home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/home/isak/Programs/rust/target/debug/build/libc-0bf4ed26e03f527c/build_script_build-0bf4ed26e03f527c.d: /home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs
-
-/home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.169/build.rs:
--- build/proc-macro2-193311ff15ffd46b/build_script_build-193311ff15ffd46b.d
+++
@@ -1,5 +0,0 @@
-/home/isak/Programs/rust/target/debug/build/proc-macro2-193311ff15ffd46b/build_script_build-193311ff15ffd46b: /home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.92/build.rs
-
-/home/isak/Programs/rust/target/debug/build/proc-macro2-193311ff15ffd46b/build_script_build-193311ff15ffd46b.d: /home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.92/build.rs
-
-/home/isak/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.92/build.rs:
